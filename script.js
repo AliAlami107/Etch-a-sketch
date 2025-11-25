66d8fec54921445d3b08f5308d9716595d753543
AliAlami107 <aal107@uit.no>
2025-11-25 08:10:20 +0100

    created grid, hover effect and button. TODO add button logic

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-const grid = document.getElementById("grid");
+// Get the container class to then later populate it 
+// with cell grid
+const size = 16; // for a 16x16 grid
+const container = document.querySelector('.container');
+
+for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener("mouseover", function(){
+        console.log('Mouse over!');
+        cell.style.backgroundColor = "red";
+    });
+
+    cell.addEventListener('mouseout', function() {
+    // Code to execute when the mouse leaves the element or its children
+        console.log('Mouse out!');
+        cell.style.backgroundColor = ''; // Reset background
+    });
+
+    container.appendChild(cell);
+}
+
+
